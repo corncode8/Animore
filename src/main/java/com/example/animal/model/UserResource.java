873c86d0e7401596932d5c4a304a0e7213a7eb8a
Haram0111 <5gkfka5@naver.com>
2023-07-22 21:42:16 +0900

    google

---
+++ src/main/java/com/example/animal/model/UserResource.java
@@ -0,0 +1,12 @@
+package com.example.animal.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserResource {
+    String id;
+    String email;
+    String nickname;
+}
