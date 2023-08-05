14bdeeebdb81760ff48b7f56c32bb0caf6752c3d
YuneeeM <dana512@naver.com>
2023-08-05 16:51:15 +0900

    현재위치 수정

--- src/main/java/umc/animore/controller/LocationController.java
+++ src/main/java/umc/animore/controller/LocationController.java
@@ -7,8 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import umc.animore.repository.LocationRepository;
 
+import java.util.Optional;
+
+import static umc.animore.config.exception.BaseResponseStatus.EMPTY_PET_ID;
+
 @RestController
-@RequestMapping("/locations")
+@RequestMapping
 public class LocationController {
     private final LocationRepository locationRepository;
 
@@ -18,17 +22,22 @@ public class LocationController {
     }
 
     //현재 위치 위도 경도 클라이언트에서 받아온거 저장
-    @PostMapping
-    public BaseResponse<Location> saveLocation(@RequestBody Location location) {
+    @PatchMapping("/locations/{locationId}")
+    public BaseResponse<Location> saveLocation(@PathVariable Long locationId,@RequestParam Double latitude, @RequestParam Double longitude) {
 
-            Location nowLocation=locationRepository.save(location);
+        Optional<Location> optionalLocation = locationRepository.findById(locationId);
 
-            return new BaseResponse<>(true,"현재 위치 저장 성공",1000,nowLocation);
+        Location nowLocation = optionalLocation.get();
+        nowLocation.setLatitude(latitude);
+        nowLocation.setLongitude(longitude);
+        nowLocation = locationRepository.save(nowLocation);
+
+        return new BaseResponse<>(true,"현재 위치 저장 성공",1000,nowLocation);
 
     }
 
 
-    @GetMapping("/{locationId}")
+    @GetMapping("/locations/{locationId}")
     public BaseResponse<Location> getLocation(@PathVariable Long locationId){
 
             Location nowLocation = locationRepository.findByLocationId(locationId);
