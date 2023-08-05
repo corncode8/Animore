6f757698e683ba2d6f847d2619f94787d92284a5
corncode8 <myeongcheol8@naver.com>
2023-08-05 15:16:45 +0900

    jenkins test

--- src/main/java/umc/animore/controller/IndexController.java
+++ src/main/java/umc/animore/controller/IndexController.java
@@ -22,7 +22,7 @@ public class IndexController {
     @GetMapping("/")
     @ResponseBody
     public String index(){
-        return "jenkins test 111111";
+        return "jenkins test 22222";
     }
 
 
